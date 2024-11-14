1f2a5311dcda9ff86724e0d6936a9fd056d71a3d
xin <102584033+xm-114514@users.noreply.github.com>
2024-11-15 05:41:23 +0900

    Create ブクマ監視.js

---
+++ ブクマ監視.js
@@ -0,0 +1,36 @@
+class Erectile {
+  async pixiv_observer(target,page=1) {
+    [target, page] = [String(target), String(page)];
+    const response = await fetch(`https://www.pixiv.net/ajax/user/${target}/illusts/bookmarks?tag=&offset=48&limit=48&rest=show&lang=ja`, {
+      headers: {"accept": "application/json"},
+      referrer: `https://www.pixiv.net/users/${target}/bookmarks/artworks?p=${page}`,
+      referrerPolicy: "strict-origin-when-cross-origin",
+      method: "GET",
+    });
+    if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+    return await response.json();
+  }
+}
+
+const compileBookmarkSummary = async(uid) => {
+  try {
+    const erectile = new Erectile();
+    let data = await erectile.pixiv_observer(uid);
+    const body = data.body;
+    if (body) {
+      let r18 = 0;
+      for (const work of body.works) work.tags.includes("R-18") && r18++;
+      const r = (x, ratio = 10) => Math.round(x * ratio) / ratio;
+      const result = {
+        target: uid,
+        body: body,
+        total: body.total,
+        r18Percentage: r((r18 / body.works.length) * 100),
+        r18CountRatioText: `${r18}/${body.works.length}`
+      };
+      return result;
+    }
+  } catch (e) { return e; }
+}
+const result = await compileBookmarkSummary( target=65461184, page=1 );
+console.log(result);
